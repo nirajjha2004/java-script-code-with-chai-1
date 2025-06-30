82a963ec3a293d413d2e287a8c2c850f30aeb5b1
nirajjha2004 <164472485+nirajjha2004@users.noreply.github.com>
2025-06-30 12:35:12 +0000

    data conversion

---
+++ 01_basics/Dataconversion.js
@@ -0,0 +1,7 @@
+let score=true;
+console.log(typeof score);
+let numscore=Number(score);
+console.log(typeof numscore);
+let string1=String(score);
+console.log(string1);
+console.log(typeof (string1));
