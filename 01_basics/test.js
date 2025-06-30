67f605bd5ebb2db0e6407d2f82c68d44a5b5dd61
nirajjha2004 <164472485+nirajjha2004@users.noreply.github.com>
2025-06-30 07:28:39 +0000

    data types

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("Niraj jha");
+const x=234;
+let accountname="niraj";
+accountbank="sbi";
+var y=90;
+let accountnum;
+accountname="Ankit";
+y=100;
+accountbank="pnb";
+console.table([x,accountname,y,accountbank,accountnum]);
+/*
+not use var because in js its isssues functional scope and block scope
+*/
